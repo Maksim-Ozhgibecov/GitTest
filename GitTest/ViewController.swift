efd818e9aa994250c68b44cb1839a145579f3cb4
Maksim Ozhgibecov <ozgimak@gmail.com>
2024-11-09 13:35:48 +0500

    Add ViewDidLoad

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -8,6 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    //
+    //
+    //
+    //
+    //
+    //
+    // 
+    
 }
 
